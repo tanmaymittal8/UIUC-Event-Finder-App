b3b67fa27e8bb9a8c055b78200a3e668d4a2c12e
Tanmay Mittal <tanmaymittal2024@gmail.com>
2026-01-19 19:24:09 -0600

    adding tabs

--- app/src/main/java/edu/uiuc/cs427app/MapsMarkerActivity.java
+++ app/src/main/java/edu/uiuc/cs427app/MapsMarkerActivity.java
@@ -11,74 +11,135 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import androidx.fragment.app.Fragment;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.material.tabs.TabLayout;
 
-/**
- * An activity that displays a Google map with multiple markers and handles click events.
- */
-public class MapsMarkerActivity extends AppCompatActivity
-        implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
-
-    // 1. Define the location constants
-    private final LatLng PERTH = new LatLng(-31.952854, 115.857342);
-    private final LatLng SYDNEY = new LatLng(-33.852, 151.211);
-    private final LatLng BRISBANE = new LatLng(-27.47093, 153.0235);
-
-    // 2. Define variables to hold the marker objects
-    private Marker markerPerth;
-    private Marker markerSydney;
-    private Marker markerBrisbane;
+public class MapsMarkerActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // Ensure this layout exists in your res/layout folder (activity_maps.xml)
         setContentView(R.layout.activity_maps);
 
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        if (mapFragment != null) {
-            mapFragment.getMapAsync(this);
+        TabLayout tabLayout = findViewById(R.id.bottomNavBar);
+
+        // Load the Map by default when app starts
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, new SupportMapFragment())
+                    .commit();
         }
-    }
 
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        // 3. Add markers to the map
-        markerPerth = googleMap.addMarker(new MarkerOptions()
-                .position(PERTH)
-                .title("Perth"));
-        markerPerth.setTag(0);
+        // Handle Tab Clicks
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                Fragment selectedFragment = null;
 
-        markerSydney = googleMap.addMarker(new MarkerOptions()
-                .position(SYDNEY)
-                .title("Sydney"));
-        markerSydney.setTag(0);
+                // Switch based on the Index (0, 1, 2)
+                switch (tab.getPosition()) {
+                    case 0:
+                        // User clicked "Map"
+                        selectedFragment = new SupportMapFragment();
+                        break;
+                    case 1:
+                        // User clicked "My Events"
+                        selectedFragment = new EventsFragment();
+                        break;
+                    case 2:
+                        // User clicked "Profile"
+                        selectedFragment = new ProfileFragment();
+                        break;
+                }
 
-        markerBrisbane = googleMap.addMarker(new MarkerOptions()
-                .position(BRISBANE)
-                .title("Brisbane"));
-        markerBrisbane.setTag(0);
+                // Swap the screen content
+                if (selectedFragment != null) {
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.fragment_container, selectedFragment)
+                            .commit();
+                }
+            }
 
-        // 4. Set the listener for marker clicks
-        googleMap.setOnMarkerClickListener(this);
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {}
 
-        // Move camera to Sydney
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(SYDNEY));
-    }
-
-    @Override
-    public boolean onMarkerClick(final Marker marker) {
-        // 5. Retrieve the data (tag) from the marker
-        Integer clickCount = (Integer) marker.getTag();
-
-        if (clickCount != null) {
-            clickCount = clickCount + 1;
-            marker.setTag(clickCount);
-
-            Toast.makeText(this,
-                    marker.getTitle() + " has been clicked " + clickCount + " times.",
-                    Toast.LENGTH_SHORT).show();
-        }
-        return false;
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {}
+        });
     }
 }
+
+
+
+
+/**
+ * An activity that displays a Google map with multiple markers and handles click events.
+ */
+//public class MapsMarkerActivity extends AppCompatActivity
+//        implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
+//
+//    // 1. Define the location constants
+//    private final LatLng PERTH = new LatLng(-31.952854, 115.857342);
+//    private final LatLng SYDNEY = new LatLng(-33.852, 151.211);
+//    private final LatLng BRISBANE = new LatLng(-27.47093, 153.0235);
+//
+//    // 2. Define variables to hold the marker objects
+//    private Marker markerPerth;
+//    private Marker markerSydney;
+//    private Marker markerBrisbane;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        // Ensure this layout exists in your res/layout folder (activity_maps.xml)
+//        setContentView(R.layout.activity_maps);
+//
+//        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+//                .findFragmentById(R.id.map);
+//        if (mapFragment != null) {
+//            mapFragment.getMapAsync(this);
+//        }
+//    }
+//
+//    @Override
+//    public void onMapReady(GoogleMap googleMap) {
+//        // 3. Add markers to the map
+//        markerPerth = googleMap.addMarker(new MarkerOptions()
+//                .position(PERTH)
+//                .title("Perth"));
+//        markerPerth.setTag(0);
+//
+//        markerSydney = googleMap.addMarker(new MarkerOptions()
+//                .position(SYDNEY)
+//                .title("Sydney"));
+//        markerSydney.setTag(0);
+//
+//        markerBrisbane = googleMap.addMarker(new MarkerOptions()
+//                .position(BRISBANE)
+//                .title("Brisbane"));
+//        markerBrisbane.setTag(0);
+//
+//        // 4. Set the listener for marker clicks
+//        googleMap.setOnMarkerClickListener(this);
+//
+//        // Move camera to Sydney
+//        googleMap.moveCamera(CameraUpdateFactory.newLatLng(SYDNEY));
+//    }
+//
+//    @Override
+//    public boolean onMarkerClick(final Marker marker) {
+//        // 5. Retrieve the data (tag) from the marker
+//        Integer clickCount = (Integer) marker.getTag();
+//
+//        if (clickCount != null) {
+//            clickCount = clickCount + 1;
+//            marker.setTag(clickCount);
+//
+//            Toast.makeText(this,
+//                    marker.getTitle() + " has been clicked " + clickCount + " times.",
+//                    Toast.LENGTH_SHORT).show();
+//        }
+//        return false;
+//    }
+//}
